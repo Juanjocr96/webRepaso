b3e2a52db08ec5e432af5f2424337018bd0958a1
Juanjocr96 <90802047+Juanjocr96@users.noreply.github.com>
2021-09-28 21:12:23 +0200

    Formulario php

---
+++ Web_Repaso/contacto.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+		
+			echo '<p>Nombre: '.$_GET["nombre"].'</p>';
+			echo '<p>Apellidos: '.$_GET["apellidos"].'</p>';
+			echo '<p>Correo: '.$_GET["correo"].'</p>';
+			echo '<p>Mensaje: '.$_GET["mensaje"].'</p>';
+			echo '<p>Motivo: '.$_GET["motivo"].'</p>';
+			echo '<p>Acepta terminos: '.$_GET["check"].'</p>';
+		?>
+	</body>
+</html>
